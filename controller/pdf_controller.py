4eb6ea82597bf43b19ea0c15bbd5d4abd8d25896
Geeks-Vegeta <shreyasmohite786@gmail.com>
2022-11-09 12:03:33 +0530

    pdfbsf added

--- controller/pdf_controller.py
+++ controller/pdf_controller.py
@@ -23,10 +23,8 @@ def pdf(file, angle_of_rotation, page_number):
         if page_number not in range(1, pdf_page_numbers+1):
             return {"message":f"input page number does not exists because this pdf only contain {pdf_page_numbers} pages"}
 
-
         writer = PdfWriter()
         return_data = io.BytesIO()
-
 
         for idxnum, page in enumerate(pdf_reader.pages):
             if idxnum == page_number-1:
@@ -36,28 +34,62 @@ def pdf(file, angle_of_rotation, page_number):
 
         with open(file_name, "wb") as pdf_out:
             writer.write(pdf_out)
-        
 
-        # This base64 online link file for streaming download
+
         with open(file_name, 'rb') as fo:
-            return_data.write(b"data:application/pdf;base64,")
-            return_data.write(base64.b64encode(fo.read()))
+            return_data.write(fo.read())
             return_data.seek(0)  
         os.remove(file_name)
 
-        return send_file(return_data,mimetype='application/pdf')
 
-        # with open(file_name, 'rb') as fo:
-        #     return_data.write(fo.read())
-        #     return_data.seek(0)  
-        # os.remove(file_name)
-    
-
-        # return send_file(return_data,as_attachment=True,mimetype='application/pdf',
-        #             download_name='download_pdf.pdf')
+        return send_file(return_data,as_attachment=True,mimetype='application/pdf',
+                    download_name='download_pdf.pdf')
  
     except Exception as e:
       print(e)
       return {"message":"Only pdf file are allowed"},400
     
 
+
+
+
+def pdfbsf(file, angle_of_rotation, page_number):
+
+    try:
+
+        document = io.BytesIO(file)
+        pdf_reader = PdfReader(document)
+        pdf_page_numbers = len(pdf_reader.pages)
+        page_number = int(page_number)
+        angle_of_rotation = int(angle_of_rotation)
+        file_name = f"abx.pdf"
+
+
+        if angle_of_rotation not in [90,180,270]:
+            return {"message":"Angle of Rotation degree must be one of these 90, 180, 270"}
+
+        if page_number not in range(1, pdf_page_numbers+1):
+            return {"message":f"input page number does not exists because this pdf only contain {pdf_page_numbers} pages"}
+
+
+        writer = PdfWriter()
+        return_data = io.BytesIO()
+        write_data = io.BytesIO()
+
+
+        for idxnum, page in enumerate(pdf_reader.pages):
+            if idxnum == page_number-1:
+                page.rotate_clockwise(angle_of_rotation)
+            writer.add_page(page)
+
+        writer.write(return_data)
+        write_data.write(b"data:application/pdf;base64,")
+        write_data.write(base64.b64encode(return_data.getvalue()))
+        write_data.seek(0)
+
+        return send_file(write_data,mimetype='application/pdf')
+ 
+    except Exception as e:
+      print(e)
+      return {"message":"Only pdf file are allowed"},400
+    
--- routes/pdf_route.py
+++ routes/pdf_route.py
@@ -5,7 +5,7 @@ an Initial HomeRoue
 
 from flask_restful import Resource, request
 import json
-from controller.pdf_controller import pdf
+from controller.pdf_controller import pdf, pdfbsf
 
 
 class PDFRoute(Resource):
@@ -14,5 +14,9 @@ class PDFRoute(Resource):
         file = request.get_data() 
         angle_of_rotation = request.args.get('angle_of_rotation')
         page_number = request.args.get('page_number')
+        base = request.args.get('isbase')
 
-        return pdf(file, angle_of_rotation, page_number)
+        if base == "True":
+            return pdfbsf(file, angle_of_rotation, page_number)
+        else:
+            return pdf(file, angle_of_rotation, page_number)
